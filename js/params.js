b1a2b47657115eed5b2d015d559c453af69f192a
Dennis Goldfarb <d.goldfarb@wustl.edu>
2020-05-06 19:08:12 -0500

    Checking if a facility type is missing from the params file

--- js/params.js
+++ js/params.js
@@ -14,6 +14,10 @@ async function processParams() {
             'medical_facilities': medicalFacilitiesParams,
         });
 
+        // sanity check on facility types.
+        const rowsWithMissingTypes = App.data.medical_facilities.filter(d => !Object.keys(params.medical_facilities).includes(d.Type));
+        rowsWithMissingTypes.forEach(d => console.error("Missing parameters for facility: " + d.Type));
+        
         return params;
     });
 }
