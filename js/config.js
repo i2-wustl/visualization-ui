6fa6c881018769c77051953f1e4e2c4dd9051fab
ITLackey <itlackey@outlook.com>
2020-05-21 15:35:08 -0500

    config can now be set during init or from url

---
+++ js/config.js
@@ -0,0 +1,124 @@
+class ConfigLoader {
+    constructor() {
+
+        const defaultConfig = {
+            "url": "config.json",
+            "data": {
+                "files": {
+                    "timeline_events": "data/timeline-events.csv",
+                    "patients": "data/patients.tsv",
+                    "medical_facilities": "data/medical-facilities.csv"
+                }
+            },
+            "overlays": {
+                "medical_facilities": [
+                    {
+                        "type": "Hospital",
+                        "size": 14,
+                        "fill": "#1f78b4"
+                    },
+                    {
+                        "type": "Drive-thru",
+                        "size": 14,
+                        "fill": "#a6cee3"
+                    },
+                    {
+                        "type": "Testing site",
+                        "size": 14,
+                        "fill": "#a6cee3"
+                    }
+                ],
+                "patients": [
+                    {
+                        "cohort": "COVID-19+",
+                        "size": 9,
+                        "fill": "#B92739",
+                        "fill-opacity": 0.75,
+                        "z-order": 2
+                    },
+                    {
+                        "cohort": "Influenza+",
+                        "size": 9,
+                        "fill": "#fb9a99",
+                        "fill-opacity": 0.5,
+                        "z-order": 1
+                    }
+                ]
+            }
+        };
+
+        return {
+            init: async (config) => {
+                const input = {
+                    ...defaultConfig
+                };
+
+                mergeConfig(input, config);
+
+                // Object.assign(input, defaultConfig, config ?? {});
+                return await processParams(input);
+            }
+        }
+
+        function mergeConfig(target, source) {
+            if (source != null) {
+
+                //Allow override to null
+                //if (source.url)
+                target.url = source.url;
+
+                if (source.data)
+                    target.data = source.data;
+
+                if (target.overlays == null)
+                    target.overlays = {};
+
+                if (source.overlays && source.overlays.medical_facilities)
+                    target.overlays.medical_facilities = source.overlays.medical_facilities;
+
+                if (source.overlays && source.overlays.patients)
+                    target.overlays.patients = source.overlays.patients;
+            }
+        }
+
+
+        async function processParams(config) {
+            //set default values
+            const output = {};
+
+            //override defaults via arg
+            //Object.assign(output, config);
+            mergeConfig(output, config);
+
+            if (config.url) {
+                const response = await fetch(config.url);
+                if (response.ok) {
+                    let json = await response.json();
+                    //Object.assign(output, json);
+                    mergeConfig(output, json);
+                }
+            }
+
+            let medicalFacilitiesParams = {};
+            output.overlays.medical_facilities.forEach(d => medicalFacilitiesParams[d.type] = d);
+
+            let patientParams = {};
+            output.overlays.patients.forEach(d => patientParams[d.cohort] = d);
+
+            // mergeConfig(output, {
+            //     'medical_facilities': medicalFacilitiesParams,
+            //     'patients': patientParams
+            // });
+
+            output.medical_facilities = medicalFacilitiesParams;
+            output.patients = patientParams;
+            return output;
+
+        }
+
+
+
+    }
+
+
+}
