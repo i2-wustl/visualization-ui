0039b2c491bd0604153012df99e544c062f10465
DennisGoldfarb <d.goldfarb@wustl.edu>
2020-05-22 23:07:31 -0500

    Sorting patient dates at initialization. Allows binary search when filtering by dates. Decreased filtering time from 15% of computation time to <1%

--- js/time-utilities.js
+++ js/time-utilities.js
@@ -24,5 +24,59 @@ function treatAsUTC(date) {
     return result;
 }
 
+function compareDates(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+}
+
+function binarySearchFirstOccurrence(haystack, needle, comparator, low, high) {
+    let mid, cmp;
+
+    if(low === undefined)
+        low = 0;
+
+    else {
+        low = low|0;
+        if(low < 0 || low >= haystack.length)
+            throw new RangeError("invalid lower bound");
+    }
+
+    if(high === undefined)
+        high = haystack.length - 1;
+
+    else {
+        high = high|0;
+        if(high < low || high >= haystack.length)
+            throw new RangeError("invalid upper bound");
+    }
+
+    while(low <= high) {
+        // The naive `low + high >>> 1` could fail for array lengths > 2**31
+        // because `>>>` converts its operands to int32. `low + (high - low >>> 1)`
+        // works for array lengths <= 2**32-1 which is also Javascript's max array
+        // length.
+        mid = low + ((high - low) >>> 1);
+        cmp = +comparator(haystack[mid], needle, mid, haystack);
+
+        // Too low.
+        if(cmp < 0.0)
+            low  = mid + 1;
+
+        // Too high.
+        else if(cmp > 0.0)
+            high = mid - 1;
+        // Equal but range is not fully scanned
+        else if (low != mid)
+            high = mid;
+        // Key found.
+        else
+            return mid;
+    }
+
+    // Key not found. -(index + 1) where index is insertion point to maintain sorted order
+    return ~low;
+}
+
 
 
